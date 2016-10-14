23aa87a23f2fc4c19ba48eaa3cd615879ea5e487
kerbinagent <wanghouze@gmail.com>
2016-10-14 13:44:30 -0500

    first commit

---
+++ tradenode/tradenode/Main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <Windows.h>
+#include <tchar.h>
+using std::string;
+int main()
+{
+	string cmd;
+	std::cout << "EU4 Trade Node Map Explorer" << std::endl;
+	while (std::cin >> cmd) {
+		
+	}
+	return 0;
+}
