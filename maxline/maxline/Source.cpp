f4b805bfd0d78c992d6f0dbce4027b624d2df5b4
kerbinagent <wanghouze@gmail.com>
2016-11-05 12:29:42 -0500

    finish

--- maxline/maxline/Source.cpp
+++ maxline/maxline/Source.cpp
@@ -1,6 +1,7 @@
 #include <vector>
 #include <list>
 #include <iostream>
+#include <algorithm>
 using std::vector;
 using std::list;
 struct Point {
@@ -20,7 +21,7 @@ public:
 			vector<float> line;
 			for (int j = 0; j < n; j++)
 			{
-				line.push_back(0);
+				line.push_back(-2);
 			}
 			slopearr.push_back(line);
 		}
@@ -30,9 +31,35 @@ public:
 			{
 				if (i == j)
 				{
-
+					slopearr[i][j] = -1;
+				}
+				else
+				{
+					if (slopearr[i][j] == -2)
+					{
+						slopearr[i][j] = abs((points[j].y - points[i].y) / (points[j].x - points[i].x));
+					}
 				}
 			}
 		}
+		int max = 0;
+		for (int i = 0; i < n; i++)
+		{
+			int count = 0;
+			std::sort(slopearr[i].begin(), slopearr[i].end());
+			for (int j = 1; j < n; j++)
+			{
+				if (slopearr[i][j] == slopearr[i][j - 1])
+				{
+					count++;
+				}
+				else
+				{
+					if (count > max) max = count;
+					count = 0;
+				}
+			}
+		}
+		return max;
 	}
 };
