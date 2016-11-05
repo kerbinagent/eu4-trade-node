284f1b57c875f27d4b308ceaeeed1f19eafced95
kerbinagent <wanghouze@gmail.com>
2016-11-05 13:48:07 -0500

    update on minus bug still unresolved

--- maxline/maxline/Source.cpp
+++ maxline/maxline/Source.cpp
@@ -16,13 +16,14 @@ public:
 	int maxPoints(vector<Point>& points) {
 		int n = points.size();
 		if (n < 2) return n;
-		vector<vector<float>> slopearr;
+		vector<vector<struct Record>> slopearr;
 		for (int i = 0; i < n; i++)
 		{
-			vector<float> line;
+			vector<struct Record> line;
 			for (int j = 0; j < n; j++)
 			{
-				line.push_back(-2);
+				struct Record rec;
+				line.push_back(rec);
 			}
 			slopearr.push_back(line);
 		}
@@ -32,21 +33,29 @@ public:
 			{
  				if (i == j)
 				{
-					slopearr[i][j] = -3;
+					slopearr[i][j].type = 0;
 				}
 				else
 				{
-					if (slopearr[i][j] == -2)
+					if (slopearr[j][i].calculated = true)
+					{
+						slopearr[i][j] = slopearr[j][i];
+					}
+					else
 					{
 						if (points[j].x - points[i].x == 0)
 						{
-							slopearr[i][j] = -1;
+							slopearr[i][j].type = 2;
 						}
 						else
 						{
-							slopearr[i][j] = abs(((float) points[j].y - (float) points[i].y) / ( (float) points[j].x - (float) points[i].x));
+							int realj, reali;
+							realj = (points[j].x > points[i].x ? j : i);
+							reali = realj == j ? i : j;
+							slopearr[i][j].slope = (((float) points[realj].y - (float) points[reali].y) / ( (float) points[realj].x - (float) points[reali].x));
+							slopearr[i][j].type = 1;
 						}
-						
+						slopearr[i][j].calculated = true;
 					}
 				}
 			}
@@ -55,10 +64,10 @@ public:
 		for (int i = 0; i < n; i++)
 		{
 			int count = 2;
-			std::sort(slopearr[i].begin(), slopearr[i].end());
+			std::sort(slopearr[i].begin(), slopearr[i].end(), customLess);
 			for (int j = 1; j < n; j++)
 			{
-				if (slopearr[i][j] == slopearr[i][j - 1])
+				if (slopearr[i][j].type == slopearr[i][j - 1].type && slopearr[i][j].slope == slopearr[i][j-1].slope)
 				{
 					count++;
 					if (count > max) max = count;
@@ -73,15 +82,30 @@ public:
 		}
 		return max;
 	}
+private:
+	struct Record {
+		int type;
+		float slope;
+		bool calculated;
+		Record() : calculated(false), slope(0) {}
+	};
+	struct {
+		bool operator()(struct Record a, struct Record b)
+		{
+			if (a.type == 0) return true;
+			if (a.type == 1) return false;
+			return (a.slope < b.slope);
+		}
+	} customLess;
 };
 
 int main() {
 	Solution sol;
 	Point pt1 = Point(0, 0);
 	Point pt2 = Point(1, 1);
-	Point pt3 = Point(2, 2);
+	Point pt3 = Point(1, -1);
 	Point pt4 = Point(4, 4);
-	vector<Point> pts = {pt1, pt2, pt3, pt4};
+	vector<Point> pts = {pt1, pt2, pt3};
 	std::cout << sol.maxPoints(pts) << std::endl;
 	return 0;
 }
